6a8e1ef4fa97c9f498b5081b263fd8eae75dbfd8
Wintory <wintory@users.noreply.github.com>
2022-03-09 19:44:37 +0700

    refactor: move file

--- src/App.tsx
+++ src/App.tsx
@@ -1,38 +1,9 @@
-import { useEffect, useState } from 'react'
-
-import OrderBook from './containers/OrderBook/OrderBook'
-import OrderForm from './containers/OrderForm/OrderForm'
-import useOrderBookStream from './containers/OrderBook/hooks/useOrderBookStream'
-
-import { ZIPMEX_LOGO } from './constants/image'
-
-import { TOrderBookStream } from './types/orderBook'
-import { TOrderForm } from './types/orderForm'
+import OrderList from './pages/OrderList'
 
 const App = () => {
-  const [orderBook, setOrderBook] = useState<TOrderBookStream>({
-    buy: [],
-    sell: [],
-  })
-  const { subscribe, addOrder } = useOrderBookStream()
-
-  useEffect(() => {
-    subscribe((data: TOrderBookStream) => {
-      setOrderBook(data)
-    })
-  }, [])
-
-  const handleSubmitOrder: TOrderForm['submitOrder'] = (side, { price, amount }) => {
-    addOrder(side, { price, amount })
-  }
-
   return (
     <div className='App'>
-      <header className='App-header'>
-        <img src={ZIPMEX_LOGO} className='App-logo' alt='logo' />
-      </header>
-      <OrderForm submitOrder={handleSubmitOrder} />
-      <OrderBook buy={orderBook.buy} sell={orderBook.sell} />
+      <OrderList />
     </div>
   )
 }
---
+++ src/pages/OrderList.tsx
@@ -0,0 +1,40 @@
+import { FC, useEffect, useState } from 'react'
+
+import { ZIPMEX_LOGO } from '../constants/image'
+
+import useOrderBookStream from '../containers/OrderBook/hooks/useOrderBookStream'
+import OrderBook from '../containers/OrderBook/OrderBook'
+import OrderForm from '../containers/OrderForm/OrderForm'
+
+import { TOrderBookStream } from '../types/orderBook'
+import { TOrderForm } from '../types/orderForm'
+
+const OrderList: FC = () => {
+  const [orderBook, setOrderBook] = useState<TOrderBookStream>({
+    buy: [],
+    sell: [],
+  })
+  const { subscribe, addOrder } = useOrderBookStream()
+
+  useEffect(() => {
+    subscribe((data: TOrderBookStream) => {
+      setOrderBook(data)
+    })
+  }, [])
+
+  const handleSubmitOrder: TOrderForm['submitOrder'] = (side, { price, amount }) => {
+    addOrder(side, { price, amount })
+  }
+
+  return (
+    <>
+      <header className='App-header'>
+        <img src={ZIPMEX_LOGO} className='App-logo' alt='logo' />
+      </header>
+      <OrderForm submitOrder={handleSubmitOrder} />
+      <OrderBook buy={orderBook.buy} sell={orderBook.sell} />
+    </>
+  )
+}
+
+export default OrderList
